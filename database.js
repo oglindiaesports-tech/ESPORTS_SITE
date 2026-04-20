1bfaac4a5df9160e7fcd67a83da1e4e97d5f2661
oglindiaesports-tech <ogl.india.esports@gmail.com>
2026-04-20 14:35:40 +0530

    Update database.js

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
 });
