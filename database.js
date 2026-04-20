e959d414660cf05d990d547b9323ba8fdcb88f8a
oglindiaesports-tech <ogl.india.esports@gmail.com>
2026-04-20 14:22:04 +0530

    Update database.js

--- database.js
+++ database.js
@@ -2,14 +2,13 @@ const mysql = require('mysql2/promise');
 const crypto = require('crypto');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT) || 3306,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
 });
 
 async function ensureTables() {
